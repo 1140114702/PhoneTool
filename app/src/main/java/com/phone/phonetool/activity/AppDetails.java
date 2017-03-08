fec6de9d65b5e99780b28815bc9827280811fcd9
zyb <1140114702@qq.com>
2017-03-08 17:30:42 +0800

    修改

--- app/src/main/java/com/phone/phonetool/activity/AppDetails.java
+++ app/src/main/java/com/phone/phonetool/activity/AppDetails.java
@@ -1,42 +1,41 @@
 package com.phone.phonetool.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import com.bigkoo.convenientbanner.ConvenientBanner;
-import com.bigkoo.convenientbanner.holder.CBViewHolderCreator;
-import com.bigkoo.convenientbanner.listener.OnItemClickListener;
 import com.phone.phonetool.BaseActivity;
 import com.phone.phonetool.R;
-import com.phone.phonetool.view.BannerImg;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * Created by zyb on 2017/3/7.
  */
 
-public class AppDetails extends BaseActivity implements View.OnClickListener, OnItemClickListener {
+public class AppDetails extends BaseActivity implements View.OnClickListener {
 
-    private ConvenientBanner banner;
-    private String[] imgUrls = {
-            "http://file.market.xiaomi.com/thumbnail/jpeg/l395/AppStore/0ff33c45a8834452120ab3aa6237938e142b1f20d",
-            "http://file.market.xiaomi.com/thumbnail/jpeg/l395/AppStore/04be22493e3b546dc1f387ba9668caf318c680043",
-            "http://file.market.xiaomi.com/thumbnail/jpeg/l395/AppStore/03be24893b3c5c6d63f388ba9708c8f013e4146a2",
-            "http://file.market.xiaomi.com/thumbnail/jpeg/l395/AppStore/03be24893b3c5d6d6ff387ba9808cdf011e4146a2"
-    };
-    private List<String> imgList = new ArrayList<>();
+//    private ConvenientBanner banner;
+//    private String[] imgUrls = {
+//            "http://file.market.xiaomi.com/thumbnail/jpeg/l395/AppStore/0ff33c45a8834452120ab3aa6237938e142b1f20d",
+//            "http://file.market.xiaomi.com/thumbnail/jpeg/l395/AppStore/04be22493e3b546dc1f387ba9668caf318c680043",
+//            "http://file.market.xiaomi.com/thumbnail/jpeg/l395/AppStore/03be24893b3c5c6d63f388ba9708c8f013e4146a2",
+//            "http://file.market.xiaomi.com/thumbnail/jpeg/l395/AppStore/03be24893b3c5d6d6ff387ba9808cdf011e4146a2"
+//    };
+//    private List<String> imgList = new ArrayList<>();
     private RelativeLayout contentMore;
     private TextView content;
-    private boolean isMore = false;
+    private boolean isMore = false, tagIsMore;
     private ImageView moreImg;
+    private ImageView tagMore;
+    private LinearLayout appInfoLayout;
+    private ImageView appIcon;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -46,13 +45,32 @@ public class AppDetails extends BaseActivity implements View.OnClickListener, On
     }
 
     private void initView() {
-        banner = (ConvenientBanner) findViewById(R.id.app_detail_banner);
+//        banner = (ConvenientBanner) findViewById(R.id.app_detail_banner);
         contentMore = (RelativeLayout) findViewById(R.id.app_detail_more);
         content = (TextView) findViewById(R.id.app_detail_content);
         moreImg = (ImageView) findViewById(R.id.app_detail_more_img);
+        tagMore = (ImageView) findViewById(R.id.app_detail_tag_more);
+        appInfoLayout = (LinearLayout) findViewById(R.id.app_detail_info_layout);
+        appIcon = (ImageView) findViewById(R.id.app_detail_icon);
+        toolbar = (Toolbar) findViewById(R.id.app_detail_toolbar);
 
         contentMore.setOnClickListener(this);
         content.setOnClickListener(this);
+        findViewById(R.id.app_detail_tag_layout_more).setOnClickListener(this);
+
+        toolbar.setTitle("QQ");
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+//        toolbar.setNavigationIcon(getResources().getDrawable(R.mipmap.ic_menu));
+//        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                finish();
+//            }
+//        });
 
         //判断简介的行数进行一些操作
         content.post(new Runnable() {
@@ -69,37 +87,39 @@ public class AppDetails extends BaseActivity implements View.OnClickListener, On
             }
         });
 
-        imgList = Arrays.asList(imgUrls);
-        banner.setPages(new CBViewHolderCreator<BannerImg>() {
-            @Override
-            public BannerImg createHolder() {
-                return new BannerImg();
-            }
-        }, imgList)
-                .setPageIndicator(new int[]{R.mipmap.ic_banner_no_select, R.mipmap.ic_banner_select})
-                .setPageIndicatorAlign(ConvenientBanner.PageIndicatorAlign.CENTER_HORIZONTAL);
+//        imgList = Arrays.asList(imgUrls);
+//        banner.setPages(new CBViewHolderCreator<BannerImg>() {
+//            @Override
+//            public BannerImg createHolder() {
+//                return new BannerImg();
+//            }
+//        }, imgList)
+//                .setPageIndicator(new int[]{R.mipmap.ic_banner_no_select, R.mipmap.ic_banner_select})
+//                .setPageIndicatorAlign(ConvenientBanner.PageIndicatorAlign.CENTER_HORIZONTAL);
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        //自动轮播
-        banner.startTurning(3000);
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        banner.stopTurning();
-    }
+    //    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        //自动轮播
+//        banner.startTurning(3000);
+//    }
 
-    @Override
-    public void onItemClick(int position) {
-        Intent intent = new Intent(this, AppDetailImgs.class);
-        intent.putExtra("imgList", imgUrls);
-        intent.putExtra("position", banner.getCurrentItem());
-        startActivity(intent);
-    }
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        banner.stopTurning();
+//    }
 
     @Override
     public void onClick(View v) {
@@ -114,6 +134,17 @@ public class AppDetails extends BaseActivity implements View.OnClickListener, On
                     isMore = false;
                     content.setMaxLines(3);
                     moreImg.setImageResource(R.mipmap.ic_more_down);
+                }
+                break;
+            case R.id.app_detail_tag_layout_more:
+                if (!tagIsMore) {
+                    tagIsMore = true;
+                    findViewById(R.id.app_detail_tag_layout).setVisibility(View.VISIBLE);
+                    tagMore.setImageResource(R.mipmap.ic_up);
+                } else {
+                    tagIsMore = false;
+                    findViewById(R.id.app_detail_tag_layout).setVisibility(View.GONE);
+                    tagMore.setImageResource(R.mipmap.ic_down);
                 }
                 break;
         }
